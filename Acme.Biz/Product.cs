cb76eef7c47101a723d9e2803349014918fbad23
Paul Meijboom <Paul Meijboom>
2018-08-31 12:46:16 +0200

    Add project files.

---
+++ Acme.Biz/Product.cs
@@ -0,0 +1,120 @@
+﻿using Acme.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{   /// <summary>
+    /////    Manages products carried.....
+    /// </summary>
+    public class Product
+    {
+#region constants
+        public const double InchesPerMeter = 39.37;
+        public readonly decimal MinimumPrice;
+#endregion
+#region constructors
+        public Product()
+        {
+            Console.WriteLine("nice product");
+            // 3) ALWAYS instantiate an instance of a vendor
+            // this.ProductVendor = new Vendor();
+            this.MinimumPrice = .96m;
+          
+        }
+        public Product(int productID, string productName, string productDescription)
+        {
+            this.ProductID = productID;
+            this.ProductName = productName;
+            this.ProductDescription = productDescription;
+            if (ProductName.StartsWith("Bulk"))
+            {
+                this.MinimumPrice = 9.99m;
+            }
+
+            Console.WriteLine("No. " + ProductID + " called: " + ProductName + " is a " + ProductDescription);
+        }
+#endregion
+#region properties
+        private DateTime? availabilityDate;
+        public DateTime? AvailabilityDate
+        {
+            get { return availabilityDate; }
+            set { availabilityDate = value; }
+        }
+
+        private int productID;
+        public int ProductID
+        {
+            get { return productID; }
+            set { productID = value; }
+        }
+
+        private string productName;
+        public string ProductName
+        {
+            get
+            {
+                var formattedValue = productName?.Trim();
+                return formattedValue;
+            }
+            set { productName = value; }
+        }
+
+        private string productDescription;
+        public string ProductDescription
+        {
+            get { return productDescription; }
+            set { productDescription = value; }
+        }
+
+        // 2) instantiating a vendor object when class 'Product' is called
+        private Vendor productVendor;
+
+        public Vendor ProductVendor
+        {
+            get
+            {
+                // lazy loading: een instance van vendor maken alleen alsie nodig is
+                if (productVendor == null)
+                {
+                    productVendor = new Vendor();
+                }
+                
+                return productVendor;
+            }
+            set { productVendor = value; }
+        }
+#endregion
+#region methods
+        public string SayHello()
+        {
+            //// 1) instantiating a vendor object ONLY when this method is called
+            //var vendor = new Vendor();
+            //vendor.SendWelcomeEmail("Message from Product");
+
+            var emailService = new EmailService();
+            var confirmation = emailService.SendMessage 
+                (
+                "New Product",
+                this.ProductName, 
+                "sales@abc.com"
+                );
+
+            // var result = LogAction("saying hello");
+        
+
+            return "Hello, your " 
+                + ProductName 
+                + " with Product number: '" 
+                + ProductID 
+                + "' is a " 
+                + ProductDescription
+                +" available on: "
+                + AvailabilityDate?.ToShortDateString();
+        }
+#endregion
+    }
+}
---
+++ Tests/Acme.BizTests/ProductTests.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+        // 3 methods to initialize an object: 
+
+        [TestMethod()]
+        public void SayHelloTest_SettingProperties1()
+        {
+            // Arrange
+            var currentProduct = new Product();
+            currentProduct.ProductName = "Saw";
+            currentProduct.ProductID = 1;
+            currentProduct.ProductDescription = "15-inch black steel blade";
+            currentProduct.ProductVendor.CompanyName = "ABC Corp";
+
+            var expected = "Hello, your Saw with Product number: '1' is a 15-inch black steel blade"
+                + " available on: ";
+
+            // Act
+            var actual = currentProduct.SayHello();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SayHelloTest_ParameterizedConstructor2()
+        {
+            // Arrange
+            var oefenProduct = new Product(32, "Pukje", "Grooot");
+            var expected = "Hello, your Pukje with Product number: '32' is a Grooot" + " available on: ";
+
+            // Act
+            var actual = oefenProduct.SayHello();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SayHelloTest_ObjectInitializer3()
+        {
+            // Arrange
+            var currentProduct = new Product
+            {
+                ProductID = 3,
+                ProductName = "boot",
+                ProductDescription = "Zeiljacht"
+            };
+
+            var expected = "Hello, your boot with Product number: '3' is a Zeiljacht" + " available on: ";
+
+            // Act
+            var actual = currentProduct.SayHello();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Product_NullTest()
+        {
+            // Arrange
+            Product currentProduct = null;
+            var companyName = currentProduct?.ProductVendor?.CompanyName;
+
+            string expected = null;
+
+            // Act
+            var actual = companyName;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ConvertMetersToInchesTest()
+        {
+            // Arrange
+            var expected = 78.74;
+
+            // Act
+            var actual = 2 * Product.InchesPerMeter;
+
+            // Assert
+
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MinimumPriceTest_Default()
+        {
+            // Arrange
+            var currentProduct = new Product();
+            var expected = .96m;
+
+            // Act
+            var actual = currentProduct.MinimumPrice;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MinimumPriceTest_Bulk()
+        {
+            // Arrange
+            var currentProduct = new Product(1, "Bulk Tools","");
+            var expected = 9.99m;
+
+            // Act
+            var actual = currentProduct.MinimumPrice;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void ProductName_Format()
+        {
+            // Arrange
+            var currentProduct = new Product();
+            currentProduct.ProductName = "   Steel Hammer   ";
+            var expected = "Steel Hammer";
+
+            // Act
+            var actual = currentProduct.ProductName;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
